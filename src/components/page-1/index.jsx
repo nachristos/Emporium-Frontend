722036c3960f3512d5186377895f2197d1afc95c
Nathan Christos <nathan.christos@Mac.gateway>
2025-07-01 08:12:19 +1000

    set up routing

---
+++ src/components/page-1/index.jsx
@@ -0,0 +1,12 @@
+export const Page1 = () => {
+  return (
+    <div className="w-full center">
+      <div>
+        <img src="./src/assets/dr-nef-1.webp" className="logo" alt="Rd Nefario" />
+        <div className='my title'>
+          Page 1
+        </div>
+      </div>
+    </div>
+  );
+}
