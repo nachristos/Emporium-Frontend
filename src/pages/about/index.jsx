c5801b78a83fa0cefc28bc3d70d853babdaa7f89
MIB80 <s3756046@student.rmit.edu.au>
2025-08-12 18:27:53 +1000

    setting up about page

---
+++ src/pages/about/index.jsx
@@ -0,0 +1,23 @@
+import './index.css'
+
+// Export----------------------------------------------------------------------------------------------------
+export const About = () => {
+  // Display
+  return (
+    <div className='wrapper'>
+        <h1 className="img-title">Nefario's Lab</h1>
+        <p>
+            Your One Stop Shop for everything nefarious! 
+        </p>
+        <p>
+            Dr Nefario is a famous mad scientist that loves making evil inventions for the world's worst villan; Dru.
+        </p>
+        <p>
+            He's sick of the lack of notiriaty because all the credit goes to the super villan he works for, he decided to sell his inventions.
+        </p>
+        <p>
+            Following he's utter frustration for being rejected to sell his marvelous evil inventions, he started his own platform; Nefario's Lab.
+        </p>
+    </div>
+  );
+}
