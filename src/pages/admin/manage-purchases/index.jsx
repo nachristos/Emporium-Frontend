7f6c5bb4ede7d552484ac674db2f0393e9a2c2c0
MIB80 <s3756046@student.rmit.edu.au>
2025-08-25 21:25:32 +1000

    Adding shipping status to purchase

--- src/pages/admin/manage-purchases/index.jsx
+++ src/pages/admin/manage-purchases/index.jsx
@@ -3,7 +3,6 @@ import { Loader } from "../../../components/shared/loader";
 import { usePurchases } from "../../../hooks/use-purchases";
 import './index.css';
 import { IconButton } from "../../../components/shared/icon-button";
-import { EditIcon } from "../../../assets/icons/edit-icon";
 import { ViewPurchase } from "../purchase/view-purchase";
 import { Button } from "../../../components/shared/button";
 import { CloseIcon } from "../../../assets/icons/close-icon";
@@ -11,6 +10,14 @@ import { CloseIcon } from "../../../assets/icons/close-icon";
 export const ManagePurchases = () => {
   const { data, loading } = usePurchases();
   const [purchase, setPurchase] = useState(false);
+
+//   const PWrapper = ({ children }) => {
+//         if (!(purchase.shipped) || purchase.shipped !== true) {
+//             return null;
+//         }
+        
+//         return <>{children}</>;
+//     };
 
   if (!data || loading) {
     return <Loader />;
@@ -48,7 +55,7 @@ export const ManagePurchases = () => {
         </div>
         {data.sort((a,b) => new Date(b.createdAt) - new Date(a.createdAt)).map(purchase => (
           <div key={purchase._id} className="item-row flex w-full text">
-            <div className="flex between w-full">
+            <div className="flex between w-full unread">
               <div className="name">
                 <h3>{(purchase.createdAt).replace(/T/, ' ').replace(/\..+/, '')}</h3>
               </div>
@@ -58,7 +65,10 @@ export const ManagePurchases = () => {
               <div className="details">
                 <h3>{`${(purchase.total/100).toFixed(2)}`}</h3>
               </div>
-              
+              {/* <PWrapper>
+                    <Button size={'small'} variant={'tertiary'} >view</Button>
+              </PWrapper> */}
+                    
               <IconButton onClick={() => setPurchase(purchase)}>
                 <Button size={'small'} variant={'secondary'} >view</Button>
               </IconButton>
--- src/pages/admin/purchase/view-purchase.jsx
+++ src/pages/admin/purchase/view-purchase.jsx
@@ -6,6 +6,12 @@ import { useQueryClient } from "@tanstack/react-query";
 export const ViewPurchase = ({ purchase, onClose, onUpdate }) => {
   const queryClient = useQueryClient();
   const data  = JSON.parse(purchase.purchase); 
+  let status = purchase.shipped;
+  const disabled = purchase.shipped;
+
+  if (purchase.shipped === true) {
+    status = 'Shipped';
+  } else {status = 'Awaiting shipping'}
 
   const { update } = useMutate(`/purchase/${purchase._id || ''}`, resp => {
         queryClient.invalidateQueries(['purchases']);
@@ -24,6 +30,10 @@ export const ViewPurchase = ({ purchase, onClose, onUpdate }) => {
     <div className="w-full listing-edit p">
       <div className="">
         <div className="mb">
+            <div className="w-full  heading"> 
+                <h3 >Shipped: </h3>
+                <h4 className="strong text p">{status}</h4>
+            </div>
             <div className="w-full  heading"> 
                 <h3 >Customer Email: </h3>
                 <h4 className="strong text p">{purchase.customerEmail}</h4>
@@ -53,7 +63,7 @@ export const ViewPurchase = ({ purchase, onClose, onUpdate }) => {
               </Button>
             </div>
             <div>
-              <Button variant="primary" className="w-full" onClick={handleSubmit}>
+              <Button disabled={disabled} variant="primary" className="w-full" onClick={handleSubmit}>
                 Mark As Shipped
               </Button>
             </div>
