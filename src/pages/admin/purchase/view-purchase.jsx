7b810b6ca2c29e8c92a767c95ce2140d3be747b1
MIB80 <s3756046@student.rmit.edu.au>
2025-08-25 16:38:23 +1000

    fixing view purchase colors

--- src/pages/admin/purchase/view-purchase.jsx
+++ src/pages/admin/purchase/view-purchase.jsx
@@ -1,4 +1,4 @@
-import { Input } from "../../../components/shared/input";
+import { useMutate } from "../../../hooks/use-mutate";
 import { Button } from "../../../components/shared/button";
 import './index.css';
 import { useQueryClient } from "@tanstack/react-query";
@@ -6,6 +6,19 @@ import { useQueryClient } from "@tanstack/react-query";
 export const ViewPurchase = ({ purchase, onClose, onUpdate }) => {
   const queryClient = useQueryClient();
   const data  = JSON.parse(purchase.purchase); 
+
+  const { update } = useMutate(`/purchase/${purchase._id || ''}`, resp => {
+        queryClient.invalidateQueries(['purchases']);
+        onUpdate(resp.slug);
+    });
+
+   const handleSubmit = () => {
+    update({
+      ...purchase,
+      shipped: true,
+    })
+    onClose();
+  }
   
   return (
     <div className="w-full listing-edit p">
@@ -13,14 +26,14 @@ export const ViewPurchase = ({ purchase, onClose, onUpdate }) => {
         <div className="mb">
             <div className="w-full  heading"> 
                 <h3 >Customer Email: </h3>
-                <h4 className="strong text">{purchase.customerEmail}</h4>
+                <h4 className="strong text p">{purchase.customerEmail}</h4>
             </div>
             <div className="w-full  heading">
               <h3>Purchased Items: </h3>
               {data.map((p, index )=> (
                 <div className="p text">
                     <div className="flex">
-                        <h3 className="strong">{` Item ${index + 1}: ${p.iname}`}</h3>
+                        <h4 className="strong">{`Item ${index + 1}: ${p.iname}`}</h4>
                     </div>
                     <h4 className="strong">{`Quantity: ${p.quantity}`}</h4>
                     <h4 className="strong">{`Price: $${(p.amount/100).toFixed(2)} each`}</h4>
@@ -30,13 +43,18 @@ export const ViewPurchase = ({ purchase, onClose, onUpdate }) => {
             </div>
             <div className="w-full  heading">
                 <h3>Total: </h3>
-                <h4 className="strong text">{(purchase.total/100).toFixed(2)}</h4>
+                <h4 className="strong text p">{`$${(purchase.total/100).toFixed(2)}`}</h4>
             </div>
         </div>
         <div className="flex between">
             <div>
               <Button variant="secondary" className="w-full" onClick={onClose}>
                 Back
+              </Button>
+            </div>
+            <div>
+              <Button variant="primary" className="w-full" onClick={handleSubmit}>
+                Mark As Shipped
               </Button>
             </div>
           </div>
